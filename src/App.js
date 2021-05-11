bd18eb9226fe1d0ac3fbfa16aaa8c55f09bf5ab3
yousseft1 <youssef.trichi3@gmail.Com>
2021-05-11 01:17:38 +0000

    update our routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage.component";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './menu-item.styles.scss';
-const MenuItem=({title,imageUrl,size})=>
+import {withRouter} from 'react-router-dom';
+const MenuItem=({title,imageUrl,size,history,linkUrl,match})=>
 {
     return (
    
-        <div className={`${size} menu-item`}>
+        <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
         <div
           className='background-image'
           style={{
@@ -20,4 +21,4 @@ const MenuItem=({title,imageUrl,size})=>
     )
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
